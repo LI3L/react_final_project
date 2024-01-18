b0ad901ba8e91f013816c2de2c57c2d4fd16301f
LI3L <khtkvg@gmail.com>
2024-01-18 16:09:41 +0200

    a

---
+++ pages/logIn.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function logIn() {
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <input type="text" name="mail" className="inputText" placeholder="enter mail"/>
+        <input type="text" name="password" className="inputText" placeholder="enter password" />
+        <button style={{margin:"10px"}}>Log In</button>
+      </div>
+    </>
+  );
+}
