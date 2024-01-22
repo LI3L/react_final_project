3545f6d64bf5986c3b096a7e9b7613276209b2bf
LI3L <khtkvg@gmail.com>
2024-01-22 21:08:59 +0200

    a

---
+++ components/ConfirmUser.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function ConfirmUser() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await axios.post(
+        "http://localhost:27017/Duolingo/api/users/login",
+        formData
+      );
+      console.log("Data sent successfully");
+    } catch (user) {
+      console.error("Error sending data:", user);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
--- pages/logIn.jsx
+++ pages/logIn.jsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
+import ConfirmUser from "../components/confirmUser";
 
 export default function logIn() {
   return (
-    <div className="logInPage">
-      <div className="login-container">
-        <div className="login-box">
-          <h2>Login</h2>
-          <form>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
+    <ConfirmUser />
+    // <div className="logInPage">
+    //   <div className="login-container">
+    //     <div className="login-box">
+    //       <h2>Login</h2>
+    //       <form>
+    //         <label htmlFor="username">Username:</label>
+    //         <input type="text" id="username" name="username" />
 
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
+    //         <label htmlFor="password">Password:</label>
+    //         <input type="password" id="password" name="password" />
 
-            <button type="submit">Login</button>
-          </form>
-        </div>
-      </div>
-    </div>
+    //         <button type="submit">Login</button>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
