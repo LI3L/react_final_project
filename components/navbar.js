6eba887d6528d36507a145bca1cc2683771b6707
Saharbabiab <117641327+Saharbabiab@users.noreply.github.com>
2024-01-22 21:08:43 +0200

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { useRouter } from "next/router";
-import Navbar from "./navbar";
-import Footer from "./footer";
+import { Navbar } from "react-bootstrap";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <>
-      <Navbar />
-      <button onClick={() => router.push("/")}>Home</button>
-      <button onClick={() => router.push("/login")}>login</button>
-      <Footer />
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">
+          {" "}
+          <h2>Home</h2>{" "}
+        </Navbar.Brand>
+        <Navbar.Brand href="/login">
+          {" "}
+          <h2>Login</h2>{" "}
+        </Navbar.Brand>
+      </Navbar>
     </>
   );
 }
